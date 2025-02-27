651f769260ec34182248302ba1a2900105b24cd0
The Mohit Soni <mohit@market-pulse.in>
2025-02-27 19:17:38 +0530

    Initial Commit

---
+++ app/src/main/java/com/mohitsoni48/preferencestatecompanionapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.mohitsoni48.preferencestatecompanionapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mohitsoni48.preferencestatecompanionapp.ui.theme.PreferenceStateCompanionAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PreferenceStateCompanionAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PreferenceStateCompanionAppTheme {
+        Greeting("Android")
+    }
+}
